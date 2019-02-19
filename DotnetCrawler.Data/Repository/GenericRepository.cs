277748d6f35ced564e3419db241dec7aaacb2fb1
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-02-19 15:40:13 +0300

    GenericRepository added

---
+++ DotnetCrawler.Data/Repository/GenericRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotnetCrawler.Data.Repository
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class, IEntity
+    {
+    }
+}
