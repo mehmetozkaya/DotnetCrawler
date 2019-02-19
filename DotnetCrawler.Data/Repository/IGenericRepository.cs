693ccc2baca379aab22b1e307d21a3055ae3171f
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-02-19 15:39:12 +0300

    IGenericRepository

---
+++ DotnetCrawler.Data/Repository/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetCrawler.Data.Repository
+{
+    public interface IGenericRepository<TEntity> where TEntity : class, IEntity
+    {
+        IQueryable<TEntity> GetAll();
+        Task<TEntity> GetById(int id);
+        Task CreateAsync(TEntity entity);
+        Task Update(int id, TEntity entity);
+        Task Delete(int id);
+    }
+}
