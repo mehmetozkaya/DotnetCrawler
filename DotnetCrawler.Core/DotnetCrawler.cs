bd2f802d9b72a0247ed6090d63061df6b7e0c04d
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-02-20 11:31:49 +0300

    DotnetCrawlerPageLinkReader

--- DotnetCrawler.Core/DotnetCrawler.cs
+++ DotnetCrawler.Core/DotnetCrawler.cs
@@ -1,5 +1,6 @@
 ﻿using DotnetCrawler.Data.Repository;
 using DotnetCrawler.Downloader;
+using DotnetCrawler.Pipeline;
 using DotnetCrawler.Processor;
 using DotnetCrawler.Request;
 using DotnetCrawler.Scheduler;
@@ -16,10 +17,54 @@ namespace DotnetCrawler.Core
         public IDotnetCrawlerDownloader Downloader { get; private set; }
         public IDotnetCrawlerProcessor<TEntity> Processor { get; private set; }
         public IDotnetCrawlerScheduler Scheduler { get; private set; }
-        //public IDotnetCrawlerPipeline<TEntity> Pipeline { get; private set; }
+        public IDotnetCrawlerPipeline<TEntity> Pipeline { get; private set; }
+
+        public DotnetCrawler()
+        {
+
+        }
+
+        public DotnetCrawler<TEntity> AddRequest(IDotnetCrawlerRequest request)
+        {
+            Request = request;
+            return this;
+        }
+
+        public DotnetCrawler<TEntity> AddDownloader(IDotnetCrawlerDownloader downloader)
+        {
+            Downloader = downloader;
+            return this;
+        }
+
+        public DotnetCrawler<TEntity> AddProcessor(IDotnetCrawlerProcessor<TEntity> processor)
+        {
+            Processor = processor;
+            return this;
+        }
+
+        public DotnetCrawler<TEntity> AddScheduler(IDotnetCrawlerScheduler scheduler)
+        {
+            Scheduler = scheduler;
+            return this;
+        }
+
+        public DotnetCrawler<TEntity> AddPipeline(IDotnetCrawlerPipeline<TEntity> pipeline)
+        {
+            Pipeline = pipeline;
+            return this;
+        }
+
         public Task Crawle()
         {
-            throw new NotImplementedException();
+            var linkReader = new DotnetCrawlerPageLinkReader(Request);
+            var links = await linkReader.GetLinks(Request.Url, 0);
+
+            foreach (var url in links)
+            {
+                var document = await Downloader.Download(url);
+                var entity = await Processor.Process(document);
+                await Pipeline.Run(entity);
+            }
         }
 
 
---
+++ DotnetCrawler.Downloader/DotnetCrawlerPageLinkReader.cs
@@ -0,0 +1,78 @@
+﻿using DotnetCrawler.Request;
+using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DotnetCrawler.Downloader
+{
+    /// <summary>
+    /// Get Urls
+    // https://codereview.stackexchange.com/questions/139783/web-crawler-that-uses-task-parallel-library 
+    /// </summary>
+    public class DotnetCrawlerPageLinkReader
+    {
+        private readonly IDotnetCrawlerRequest _request;
+        private readonly Regex _regex;
+
+        public DotnetCrawlerPageLinkReader(IDotnetCrawlerRequest request)
+        {
+            _request = request;
+            if (!string.IsNullOrWhiteSpace(request.Regex))
+            {
+                _regex = new Regex(request.Regex);
+            }
+        }
+
+        public async Task<IEnumerable<string>> GetLinks(string url, int level = 0)
+        {
+            if (level < 0)
+                throw new ArgumentOutOfRangeException(nameof(level));
+
+            var rootUrls = await GetPageLinks(url, false);
+
+            if (level == 0)
+                return rootUrls;
+
+            var links = await GetAllPagesLinks(rootUrls);
+
+            --level;
+            var tasks = await Task.WhenAll(links.Select(link => GetLinks(link, level)));
+            return tasks.SelectMany(l => l);
+        }
+
+        private async Task<IEnumerable<string>> GetPageLinks(string url, bool needMatch = true)
+        {
+            try
+            {
+                HtmlWeb web = new HtmlWeb();
+                var htmlDocument = await web.LoadFromWebAsync(url);
+
+                var linkList = htmlDocument.DocumentNode
+                                   .Descendants("a")
+                                   .Select(a => a.GetAttributeValue("href", null))
+                                   .Where(u => !string.IsNullOrEmpty(u))
+                                   .Distinct();
+
+                if (_regex != null)
+                    linkList = linkList.Where(x => _regex.IsMatch(x));
+
+                return linkList;
+            }
+            catch (Exception exception)
+            {
+                return Enumerable.Empty<string>();
+            }
+        }
+
+        private async Task<IEnumerable<string>> GetAllPagesLinks(IEnumerable<string> rootUrls)
+        {
+            var result = await Task.WhenAll(rootUrls.Select(url => GetPageLinks(url)));
+
+            return result.SelectMany(x => x).Distinct();
+        }
+    }
+}
