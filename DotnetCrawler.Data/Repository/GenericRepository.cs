1c72be37d5da5258a9bd0fb83bf26fee8833d354
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-02-20 07:46:41 +0300

    GenericRepository implemented

--- DotnetCrawler.Data/Repository/GenericRepository.cs
+++ DotnetCrawler.Data/Repository/GenericRepository.cs
@@ -1,10 +1,52 @@
-﻿using System;
+﻿using DotnetCrawler.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace DotnetCrawler.Data.Repository
 {
     public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class, IEntity
     {
+        private readonly MicrosofteShopOnWebCatalogDbContext _dbContext;
+
+        public GenericRepository(MicrosofteShopOnWebCatalogDbContext dbContext)
+        {
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+
+        public IQueryable<TEntity> GetAll()
+        {
+            return _dbContext.Set<TEntity>().AsNoTracking();
+        }
+
+        public async Task<TEntity> GetById(int id)
+        {
+            return await _dbContext.Set<TEntity>()
+                        .AsNoTracking()
+                        .FirstOrDefaultAsync(e => e.Id == id);
+        }
+
+        public async Task CreateAsync(TEntity entity)
+        {
+            await _dbContext.Set<TEntity>().AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task Update(int id, TEntity entity)
+        {
+            _dbContext.Set<TEntity>().Update(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task Delete(int id)
+        {
+            var entity = await GetById(id);
+            _dbContext.Set<TEntity>().Remove(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
     }
 }
