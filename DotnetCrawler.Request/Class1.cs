4c28d83486413718f9486f090b0146e05ba374b7
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-02-20 09:31:03 +0300

    DotnetCrawlerRequest

--- DotnetCrawler.Request/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace DotnetCrawler.Request
-{
-    public class Class1
-    {
-    }
-}
---
+++ DotnetCrawler.Request/DotnetCrawlerRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotnetCrawler.Request
+{
+    public class DotnetCrawlerRequest : IDotnetCrawlerRequest
+    {
+        public string Url { get; set; }
+        public string Regex { get; set; }
+        public long TimeOut { get; set; }
+    }
+}
