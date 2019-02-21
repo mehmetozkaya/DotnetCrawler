9c64049b39010727a7a0b7dc9188ab9084df9fd0
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-02-21 11:27:08 +0300

    DotnetCrawlerRequest ctor added

--- DotnetCrawler.Request/DotnetCrawlerRequest.cs
+++ DotnetCrawler.Request/DotnetCrawlerRequest.cs
@@ -9,5 +9,10 @@ namespace DotnetCrawler.Request
         public string Url { get; set; }
         public string Regex { get; set; }
         public long TimeOut { get; set; }
+
+        public DotnetCrawlerRequest(string url)
+        {
+            Url = url;
+        }
     }
 }
